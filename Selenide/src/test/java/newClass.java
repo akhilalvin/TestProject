bc9cfa18c326f80a8195b06f8b9ff11badae0dde
Akhil <akhilseith@gmail.com>
2021-05-14 22:23:38 +1200

    added additional checks

--- Selenide/src/test/java/newClass.java
+++ Selenide/src/test/java/newClass.java
@@ -4,6 +4,11 @@ public class newClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("code1");
+   
+   System.out.println("code1jkb");
+   System.out.println("code2");
+   System.out.println("codejh");
+   
 	}
 
 }
