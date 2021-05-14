0815895a97d82a11a16512432802043edfb4db9e
akhilalvin <67248564+akhilalvin@users.noreply.github.com>
2021-05-14 22:32:35 +1200

    updated new class

--- Selenide/src/test/java/newClass.java
+++ Selenide/src/test/java/newClass.java
@@ -5,9 +5,11 @@ public class newClass {
 		// TODO Auto-generated method stub
    System.out.println("code1");
    
-   System.out.println("code1jkb");
+   System.out.println("code1j daskn kb");
    System.out.println("code2");
    System.out.println("codejh");
+		 System.out.println("code818jh");
+		 System.out.println("codejh992");
    
 	}
 
